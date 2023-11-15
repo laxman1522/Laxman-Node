77a72d66851fdb4764b6716bcf0b39250e7b64cb
laxman1522 <111977197+laxman1522@users.noreply.github.com>
2023-11-15 18:43:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-var randomColorPalatte = require('./randomColorPalatte');
 
-const http = require('http');
-
-const server = http.createServer((req,res) => {
-    if(req.url === '/generate-random-color') {
-        res.write(JSON.stringify(randomColorPalatte.generateRandomColorPalatte()));
-        res.end()
-    }
-});
-
-server.listen(3000);
-console.log("Listening on port 3000...")
