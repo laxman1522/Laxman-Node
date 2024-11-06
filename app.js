b0dcc60ee31008c83d8271efd4c2e743626510e6
Laxmanapandi Baskaran <laxmanapandi.baskaran@cdw.com>
2024-11-06 16:38:08 +0530

    adding user route

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import UserRoute from './src/routes/userRoute.js';
 
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT;
+
+// dotenv.config();
+
+app.use('/', UserRoute);
+
+app.listen(PORT, () => {
+    console.log(`[server]: Server is running at http://localhost:${PORT}`); 
+}); 
---
+++ src/routes/userRoute.js
@@ -0,0 +1,14 @@
+
+import express from "express";
+
+const UserRoute = express.Router();
+
+UserRoute.post('/signUp', async (req, res) => {
+
+})
+
+UserRoute.post('/login', async (req,res) => {
+
+})
+
+ export default UserRoute;
